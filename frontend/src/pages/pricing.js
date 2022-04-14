f2a9c8d3a34b31e7e70a1b9d82448228feae94d6
Alex Manzi <alex.manzi22@gmail.com>
2022-04-14 13:46:13 -0400

    FE setup

---
+++ frontend/src/pages/pricing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './pricing.css';
+
+function pricing() {
+  return (
+    <div>pricing</div>
+  )
+}
+
+export default pricing
---
+++ frontend/src/pages/results.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './results.css';
+
+function results() {
+  return (
+    <div>results</div>
+  )
+}
+
+export default results
