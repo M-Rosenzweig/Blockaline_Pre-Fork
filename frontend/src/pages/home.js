b33d4e5b70e2bd59524caa9b6192a8b629972f6e
Moshe <moisherosenzweig@gmail.com>
2022-04-18 15:51:53 -0400

    Select_Basics

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './home.css';
+
+// useEffect(() => {
+//   document.body.style.overflow = 'hidden'
+// }[])
 
 function Home() {
   return (
     <div className='master'>
-    
-      <div className='message'>
+
       <h1 className='Title'> Don't Work - Align </h1>
+      <h2 className='subtitle'> Align Your Values With The Blockchain</h2>
+      <div className='selectContainer'>
+        <select className='select' name="Frontier" required>
+        <option value="" disabled selected hidden>Choose Your Frontier</option>
+        <option value="coffee">Data Science</option>
+        <option value="tea">Software Engineering</option>
+        <option value="Cyber Se">Cyber Security</option>
+        <option value="milk">UX Design</option>
+    </select>
       </div>
-   
-
-
+        
 
     </div>
+    
   )
 }
 
