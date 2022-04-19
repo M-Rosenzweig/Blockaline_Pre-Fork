11e170156e7947fa4bd206283f396f2802393c47
Moshe <moisherosenzweig@gmail.com>
2022-04-19 10:53:31 -0400

    login_BE_Start

--- app/controllers/seekers_controller.rb
+++ app/controllers/seekers_controller.rb
@@ -1,2 +1,21 @@
 class SeekersController < ApplicationController
+    def create
+        seeker = Seeker.create!(params.permit(:name, :email, :profession, :resume))
+        session[:seeker_id] ||= seeker.id
+        render json: seeker, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+        render json: { errors: [invalid.record.errors] }, status: :unprocessable_entity
+    end
+
+    def show
+        seeker_id = session[:seeker_id]
+        if seeker_id
+            seeker = Seeker.find(seeker_id)
+            render json: seeker, status: :created
+        else
+            render json: { error: "Unauthorized" }, status: :unauthorized
+        end
+    end
+
+
 end
